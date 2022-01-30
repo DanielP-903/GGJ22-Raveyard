30fd800427da52af8988738d2d9d29cbe94c417b
Daniel Paul <danpaul309@gmail.com>
2022-01-30 00:00:41 +0000

    Pushed main and main menu changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Grid
@@ -14,18 +15,44 @@ public class Grid
 
 public class GameManager : MonoBehaviour
 {
+    public const int LEVEL = 2;
     private int m_noOfTilesOn = 0;
     private bool m_winState;
     internal int m_moves = 0;
     internal bool m_isPaused = false;
     [SerializeField] private TMP_Text m_movesText;
     [SerializeField] private Slider m_raveometer;
+    [SerializeField] private GameObject m_winPanel;
     [SerializeField] private GameObject m_TileObject;
     [SerializeField] private List<GameObject> m_foliageObjects;
     private Grid m_grid = new Grid();
+
+    private readonly int[,] m_layoutLevel1 = {
+        {0,0,1,1,1}, 
+        {1,1,1,1,1},
+        {0,0,1,1,0},
+        {0,1,1,0,0},
+        {1,1,1,1,0}
+    };
+    private readonly int[,] m_layoutLevel2 = {
+        {1,1,1,1,1},
+        {1,1,1,1,1},
+        {0,1,0,1,0},
+        {1,1,1,1,1},
+        {1,1,1,1,1}
+    }; 
+    private readonly int[,] m_layoutLevel3 = {
+        {0,0,1,1,0},
+        {1,1,0,0,1},
+        {0,1,1,1,0},
+        {1,0,0,1,0},
+        {1,0,1,0,1}
+    };
     // Start is called before the first frame update
     void Start()
     {
+        m_isPaused = false;
+        m_winState = false;
         for (int x = 0; x < Grid.WIDTH; x++)
         {
             for (int y = 0; y < Grid.HEIGHT; y++)
@@ -37,6 +64,39 @@ public class GameManager : MonoBehaviour
                 m_grid.m_tiles[x, y].GetComponent<Tile>().m_position = new Vector2Int(x,y);
                 GameObject chosenFoliage = m_foliageObjects[Random.Range(0, m_foliageObjects.Count)];
                 m_grid.m_tiles[x, y].GetComponent<Tile>().m_foliage = Instantiate(chosenFoliage, m_grid.m_tiles[x, y].transform);
+                switch (LEVEL)
+                {
+                    case 1:
+                    {
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().state = m_layoutLevel1[x, y];
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().previousState = m_layoutLevel1[x, y];
+                        if (m_layoutLevel1[x, y] == 1)
+                        {
+                            m_noOfTilesOn++;
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().state = m_layoutLevel2[x, y];
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().previousState = m_layoutLevel2[x, y];
+                        if (m_layoutLevel2[x, y] == 1)
+                        {
+                            m_noOfTilesOn++;
+                        }
+                        break;
+                    }
+                    case 3:
+                    {
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().state = m_layoutLevel3[x, y];
+                        m_grid.m_tiles[x, y].GetComponent<Tile>().previousState = m_layoutLevel3[x, y];
+                        if (m_layoutLevel3[x, y] == 1)
+                        {
+                            m_noOfTilesOn++;
+                        }
+                        break;
+                    }
+                }
                 if (x + 1 < Grid.WIDTH)
                 {
                     m_grid.m_tiles[x, y].GetComponent<Tile>().m_neighbours.Add(new Vector2Int(1,0));
@@ -59,8 +119,19 @@ public class GameManager : MonoBehaviour
 
     void Update()
     { 
-        m_raveometer.value = Mathf.Lerp(m_raveometer.value, m_noOfTilesOn / 25.0f, Time.deltaTime*2);
+        m_raveometer.value = Mathf.Lerp(m_raveometer.value, m_noOfTilesOn / 25.0f, Time.deltaTime*3);
         m_movesText.text = "Moves: " + m_moves;
+
+        if (m_raveometer.value > 0.99f)
+        {
+            m_winState = true;
+        }
+
+        if (m_winState)
+        {
+            m_winPanel.SetActive(true);
+            m_isPaused = true;
+        }
     }
 
     public void HandleInteraction(Vector2Int pos)
@@ -77,5 +148,9 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 
 }
